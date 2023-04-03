6e417d8a83c61df8e552d8340806aecbc1023948
johns <johns@Johnsy>
2023-04-03 16:16:55 +0530

    commit 1 tp anjali

---
+++ src/test/java/Practice/Example1.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Example1 {
+	@Test
+	public void ex1()
+	{
+		System.out.println("Example1");
+	}
+
+	public void ex2()
+	{
+		System.out.println("Example2");
+	}
+}
