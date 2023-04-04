36ab311c537b9246f9a01fdfacc71abb9680e136
karan <akratisahu1234@gmail.com>
2023-04-04 13:24:39 +0530

    commit56

--- src/test/java/Practice/Example2.java
+++ src/test/java/Practice/Example2.java
@@ -5,6 +5,7 @@ public class Example2 {
 	public static void main(String[] args) {
 	System.out.println("project is updated");
 System.out.println("project is not updated");
+System.out.println("project");
 	}
 
 }
